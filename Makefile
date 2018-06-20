b07f81f81d8cc07a6d5253dcbc1425fb37f2865a
Brian Ketelsen <bketelsen@gmail.com>
2018-06-20 09:06:24 -0400

    settings and makefile update

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
+wasm:
+	GOROOT=~/gowasm GOARCH=wasm GOOS=js ~/gowasm/bin/go build -o example.wasm .
+
 build-server:
 	go build -o server-app server/server.go
 
-run: build-server
+run: build-server wasm
 	./server-app
-
-wasm:
-	GOROOT=~/gowasm GOARCH=wasm GOOS=js ~/gowasm/bin/go build -o example.wasm .
-	
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+	"time"
 )
 
 func main() {
@@ -20,5 +21,15 @@ func main() {
 	todolist.Todos = []Todo{todo, todo2}
 	err := todolist.Render()
 	fmt.Println(err)
+	fmt.Println("Sleeping for 5")
+	time.Sleep(5 * time.Second)
 
+	todo3 := Todo{
+		Title: "Take a Nap",
+	}
+
+	fmt.Println("Adding another task")
+	todolist.Todos = append(todolist.Todos, todo3)
+	err = todolist.Render()
+	fmt.Println(err)
 }
